a9643f59641e0e3c947b6482b319c3cc2407109d
Oron Paz <oron.paz26@gmail.com>
2025-04-18 10:36:29 +0300

    working E1 user data collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+#tested api connection dont need this
 response = requests.get("https://dummyjson.com/test")
 print (response.json())
 
@@ -13,7 +14,7 @@ def connectivity_test(username: str, password: str):
         "password": password
     }
 
-    response = requests.post('https://dummyjson.com/auth/login', json = payload)
+    response = requests.post('https://dummyjson.com/auth/login', json=payload)
     if response.status_code == 200:
         data = response.json()
         print("Response data:", data)
@@ -23,5 +24,30 @@ def connectivity_test(username: str, password: str):
         print(f"Response: {response.text}")
         return None
 
+def collect_evidence(accessToken):
+    headers = {
+        "Authorization": accessToken,
+        "Content-Type": "application/json"
+    }
+
+    def collect_E1_userDetails():
+        response = requests.get('https://dummyjson.com/auth/me', headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to collect user details: {response.status_code}")
+            return None
+        
+    def collect_E2_posts():
+        pass
+
+    return collect_E1_userDetails()
+
 valid_login = connectivity_test("emilys", "emilyspass")
-invalid_login = connectivity_test("wrong", "wrong")
+
+E1 = collect_evidence(valid_login['accessToken'])
+print(E1)
+
+
+#dont need this anymore
+#invalid_login = connectivity_test("wrong", "wrong")
